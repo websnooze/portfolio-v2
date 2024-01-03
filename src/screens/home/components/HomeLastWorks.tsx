01d340c6efe6d478a91e7d3c00b8760cbd9f5a31
kozamonq <mathiaslac@gmail.com>
2024-01-03 15:12:58 +0100

    seo

--- src/screens/home/components/HomeLastWorks.tsx
+++ src/screens/home/components/HomeLastWorks.tsx
@@ -1,19 +1,31 @@
+import { useTranslation } from "react-i18next";
+import { motion } from "framer-motion";
+
 import { homeLastWorksData } from "./data/homeLastWorksData";
 
 import "./css/module.last-works.css";
-import { useTranslation } from "react-i18next";
 
 const HomeLastWorks = () => {
   const { t } = useTranslation();
   const data = homeLastWorksData;
   return (
     <section className="home-lastworks container">
-      {data.map((item) => (
-        <div key={item.id} className="home-lastworks-item">
+      {data.map((item, index) => (
+        <motion.div
+          key={item.id}
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -20 }}
+          transition={{
+            duration: 0.2,
+            delay: index * 0.2,
+            ease: "linear",
+          }}
+          className="home-lastworks-item">
           <div className="home-lastworks-item__icon">{item.icon}</div>
           <h4>{t(item.title)}</h4>
           <p>{t(item.text)}</p>
-        </div>
+        </motion.div>
       ))}
     </section>
   );
